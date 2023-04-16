4e4c661e100cbb44bb850ac4eb763cb872c9bb54
dedins <andrea.dedominicis.93@gmail.com>
2023-04-16 16:29:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-# Configure the Cloudflare provider using the required_providers stanza
-# required with Terraform 0.13 and beyond. You may optionally use version
-# directive to prevent breaking changes occurring unannounced.
 terraform {
   required_providers {
     cloudflare = {
