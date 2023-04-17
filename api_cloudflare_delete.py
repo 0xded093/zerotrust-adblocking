b84d2c83ff8e904d47f6d882643db28b8443eefc
dedins <andrea.dedominicis.93@gmail.com>
2023-04-17 18:15:53 +0200

    Update api_cloudflare_delete.py

--- api_cloudflare_delete.py
+++ api_cloudflare_delete.py
@@ -6,23 +6,30 @@ CLOUDFLARE_TEAM_ID = os.getenv('CLOUDFLARE_TEAM_ID')
 CLOUDFLARE_AUTH_EMAIL = os.getenv('CLOUDFLARE_AUTH_EMAIL')
 CLOUDFLARE_AUTH_KEY = os.getenv('CLOUDFLARE_AUTH_KEY')
 
-url = "https://api.cloudflare.com/client/v4/accounts/"+CLOUDFLARE_TEAM_ID+"/gateway/lists/"
+print("[+] Getting current rules")
+url = "https://api.cloudflare.com/client/v4/accounts/"+CLOUDFLARE_TEAM_ID+"/gateway/rules/"
 headers = {
     "Content-Type": "application/json",
     "X-Auth-Email": CLOUDFLARE_AUTH_EMAIL,
     "X-Auth-Key": CLOUDFLARE_AUTH_KEY
 }
-
 response = requests.request("GET", url, headers=headers)
 print(response.text)
 
-try:
-    for policy in json.loads(response.text)['result']:
-        if policy["name"] == "DNS Blocklist":
-            url = url + policy["id"] 
-            response = requests.request("DELETE", url, headers=headers)
-            print(response.text)
-        else:
-            break
-except:
-    pass
+for rule in json.loads(response.text)['result']:
+    if rule["name"] == "DNS Blocklist":
+        url = url + rule["id"] 
+        print("[-] Deleting rule:", rule["id"])
+        response = requests.request("DELETE", url, headers=headers)
+        print(response.text)
+
+print("[+] Getting current lists")
+url = "https://api.cloudflare.com/client/v4/accounts/"+CLOUDFLARE_TEAM_ID+"/gateway/lists/"
+response = requests.request("GET", url, headers=headers)
+print(response.text)
+
+for list in json.loads(response.text)['result']:
+        url2 = url + list["id"] 
+        print("[-] Deleting list:", list["id"])
+        response = requests.request("DELETE", url2, headers=headers)
+        print(response.text)
